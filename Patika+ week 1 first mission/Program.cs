75c1d68c286616d9a95e31728e2c49212594cc2f
canbe <canbe@CANBERK>
2024-07-15 20:05:56 +0300

    Add project files.

---
+++ Patika+ week 1 first mission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CanberkDogan
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Canberk Dogan");
+            Console.WriteLine("Patika+ ile birlikte yazılım sektörüne hazır ve donanımlı bir şekilde giriş yapmak istiyorum. Buradan başarılı ve memnun bir şekilde ayrılmayı hedefliyorum");
+        }
+
+    }
+
+
+
+}
